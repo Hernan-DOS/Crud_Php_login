1553ce65c6a67ba1a1fc7327866d87cb34cb830f
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2025-07-02 22:31:54 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $host = "localhost";
-$usuario = "root";
-$contrasena = "";
+$usuario = "admin";
+$contrasena = "1234";
 $base_datos = "instituto";
 $conn = new mysqli($host, $usuario, $contrasena, $base_datos);
 if ($conn->connect_error) {
